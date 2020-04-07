00a025f242b8ddf2203cfd75bfb882746b8ae24c
rladm <rladm@DESKTOP-PC70IB6>
2020-04-07 13:00:55 +0900

    2020.04.07 13:00

--- src/main/java/com/kh/ReaderForChildren/member_ej/controller/MemberController.java
+++ src/main/java/com/kh/ReaderForChildren/member_ej/controller/MemberController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.kh.ReaderForChildren.member_ej.model.exception.MemberException;
 import com.kh.ReaderForChildren.member_ej.model.service.MemberService;
 import com.kh.ReaderForChildren.member_ej.model.vo.Member;
 
+@SessionAttributes("loginUser")
 @Controller
 public class MemberController {
 	
@@ -20,6 +23,13 @@ public class MemberController {
 	public String memberLogin(Member m, Model model) {
 		
 		Member loginUser = mService.memberLogin(m);
+		
+		if(loginUser != null) {
+			model.addAttribute("loginUser", loginUser);
+			return "redirect:home.do";
+		}else {
+			throw new MemberException("로그인에 실패하였습니다.");
+		}
 		
 	}
 	
--- src/main/java/com/kh/ReaderForChildren/member_ej/model/service/MemberServiceImpl.java
+++ src/main/java/com/kh/ReaderForChildren/member_ej/model/service/MemberServiceImpl.java
@@ -1,5 +1,26 @@
 package com.kh.ReaderForChildren.member_ej.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.ReaderForChildren.member_ej.model.dao.MemberDAO;
+import com.kh.ReaderForChildren.member_ej.model.vo.Member;
+
+@Service("mService")
 public class MemberServiceImpl implements MemberService {
+	
+	@Autowired
+	private MemberDAO mDAO;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
+	
+	@Override
+	public Member memberLogin(Member m) {
+		return mDAO.selectMember(sqlSession, m);
+	}
 
 }
